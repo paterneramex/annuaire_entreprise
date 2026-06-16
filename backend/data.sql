c79828f88881ef6d3986ce9d09b03f154bae9ada
paterneramex <paterne.ramex@gmail.com>
2026-06-16 14:10:38 +0300

    rajout du fichier de population de la base de données data.sql

---
+++ backend/data.sql
@@ -0,0 +1,181 @@
+INSERT INTO employes_employe (matricule, nom, prenom, agence, poste, telephone) VALUES
+
+('101', 'Lefevre', 'Lucas', 'MSY', 'Dev IT', '+33612345678'),
+
+('102', 'Morel', 'Léa', 'WMN', 'Chef Projet', '+33623456789'),
+
+('105', 'Girard', 'Hugo', 'MKR', 'RH', '+33634567890'),
+
+('108', 'Andre', 'Emma', 'TMM', 'Data Analyst', '+33645678901'),
+
+('112', 'Mercier', 'Arthur', 'ANM', 'Conseiller', '+33656789012'),
+
+('115', 'Dupont', 'Manon', 'APA', 'Dev IT', '+33667890123'),
+
+('120', 'Lambert', 'Louis', 'FNR', 'Chef Projet', '+33678901234'),
+
+('124', 'Bonnet', 'Chloé', 'SBV', 'RH', '+33689012345'),
+
+('131', 'Francois', 'Nathan', 'ABE', 'Data Analyst', '+33690123456'),
+
+('135', 'Rousseau', 'Inès', 'MSY', 'Conseiller', '+33611112222'),
+
+('142', 'Guerin', 'Enzo', 'WMN', 'Dev IT', '+33622223333'),
+
+('147', 'Boyer', 'Jade', 'MKR', 'Chef Projet', '+33633334444'),
+
+('153', 'Chevalier', 'Leo', 'TMM', 'RH', '+33644445555'),
+
+('158', 'Mathieu', 'Lina', 'ANM', 'Data Analyst', '+33655556666'),
+
+('162', 'Fontaine', 'Gabriel', 'APA', 'Conseiller', '+33666667777'),
+
+('167', 'Masson', 'Mila', 'FNR', 'Dev IT', '+33677778888'),
+
+('171', 'Robin', 'Jules', 'SBV', 'Chef Projet', '+33688889999'),
+
+('175', 'Rivet', 'Alice', 'ABE', 'RH', '+33699990000'),
+
+('180', 'Gautier', 'Tom', 'MSY', 'Data Analyst', '+33612312312'),
+
+('184', 'Morin', 'Zoe', 'WMN', 'Conseiller', '+33623423423'),
+
+('191', 'Meyer', 'Mathis', 'MKR', 'Dev IT', '+33634534534'),
+
+('195', 'Blanchet', 'Eva', 'TMM', 'Chef Projet', '+33645645645'),
+
+('201', 'Dumont', 'Timéo', 'ANM', 'RH', '+33656756756'),
+
+('208', 'Gérard', 'Romane', 'APA', 'Data Analyst', '+33667867867'),
+
+('214', 'Nguyen', 'Raphaël', 'FNR', 'Conseiller', '+33678978978'),
+
+('220', 'Brunet', 'Clara', 'SBV', 'Dev IT', '+33689089089'),
+
+('227', 'Barbier', 'Maxime', 'ABE', 'Chef Projet', '+33690190190'),
+
+('233', 'Giraud', 'Maëlys', 'MSY', 'RH', '+33611223344'),
+
+('241', 'Picard', 'Antoine', 'WMN', 'Data Analyst', '+33622334455'),
+
+('248', 'Maurin', 'Lucie', 'MKR', 'Conseiller', '+33633445566'),
+
+('252', 'Faure', 'Clément', 'TMM', 'Dev IT', '+33644556677'),
+
+('259', 'Perrin', 'Lou', 'ANM', 'Chef Projet', '+33655667788'),
+
+('264', 'Gauthier', 'Alexandre', 'APA', 'RH', '+33666778899'),
+
+('271', 'Nicolas', 'Elsa', 'FNR', 'Data Analyst', '+33677889900'),
+
+('277', 'Raimbault', 'Paul', 'SBV', 'Conseiller', '+33688990011'),
+
+('283', 'Caron', 'Sarah', 'ABE', 'Dev IT', '+33699001122'),
+
+('290', 'Vidal', 'Valentin', 'MSY', 'Chef Projet', '+33612345612'),
+
+('296', 'Rousel', 'Margaux', 'WMN', 'RH', '+33623456723'),
+
+('302', 'Colin', 'Maxence', 'MKR', 'Data Analyst', '+33634567834'),
+
+('309', 'Aubry', 'Louna', 'TMM', 'Conseiller', '+33645678945'),
+
+('315', 'Bouchet', 'Guillaume', 'ANM', 'Dev IT', '+33656789056'),
+
+('322', 'Renard', 'Agathe', 'APA', 'Chef Projet', '+33667890167'),
+
+('328', 'Benoit', 'Augustin', 'FNR', 'RH', '+33678901278'),
+
+('334', 'Leroy', 'Juliette', 'SBV', 'Data Analyst', '+33689012389'),
+
+('341', 'Roy', 'Corentin', 'ABE', 'Conseiller', '+33690123490'),
+
+('347', 'Marchand', 'Camille', 'MSY', 'Dev IT', '+33655512345'),
+
+('355', 'Dufour', 'Sacha', 'WMN', 'Chef Projet', '+33655523456'),
+
+('361', 'Meunier', 'Lise', 'MKR', 'RH', '+33655534567'),
+
+('368', 'Brun', 'Mathéo', 'TMM', 'Data Analyst', '+33655545678'),
+
+('374', 'Gaillard', 'Jeanne', 'ANM', 'Conseiller', '+33655556789'),
+
+('385', 'Arnaud', 'Bastien', 'APA', 'Dev IT', '+33655567890'),
+
+('392', 'Gillo', 'Noémie', 'FNR', 'Chef Projet', '+33655578901'),
+
+('399', 'Vallee', 'Alexis', 'SBV', 'RH', '+33655589012'),
+
+('405', 'Paulin', 'Marion', 'ABE', 'Data Analyst', '+33655590123'),
+
+('421', 'Cousin', 'Théo', 'MSY', 'Conseiller', '+33644412345'),
+
+('428', 'Barre', 'Lana', 'WMN', 'Dev IT', '+33644423456'),
+
+('435', 'Gros', 'Marius', 'MKR', 'Chef Projet', '+33644434567'),
+
+('442', 'Marti', 'Elena', 'TMM', 'RH', '+33644445678'),
+
+('451', 'Bailly', 'Alban', 'ANM', 'Data Analyst', '+33644456789'),
+
+('458', 'Chauvin', 'Lya', 'APA', 'Conseiller', '+33644467890'),
+
+('465', 'Leclerc', 'Noa', 'FNR', 'Dev IT', '+33644478901'),
+
+('472', 'Fleury', 'Lilou', 'SBV', 'Chef Projet', '+33644489012'),
+
+('480', 'Robert', 'Mathias', 'ABE', 'RH', '+33644490123'),
+
+('487', 'Da Silva', 'Manon', 'MSY', 'Data Analyst', '+33633312345'),
+
+('493', 'Rousse', 'Loan', 'WMN', 'Conseiller', '+33633323456'),
+
+('502', 'Jacquet', 'Inès', 'MKR', 'Dev IT', '+33633334567'),
+
+('511', 'Collet', 'Evan', 'TMM', 'Chef Projet', '+33633345678'),
+
+('518', 'Prevost', 'Maé', 'ANM', 'RH', '+33633356789'),
+
+('524', 'Millet', 'Yanis', 'APA', 'Data Analyst', '+33633367890'),
+
+('531', 'Donat', 'Lola', 'FNR', 'Conseiller', '+33633378901'),
+
+('538', 'Sales', 'Esteban', 'SBV', 'Dev IT', '+33633389012'),
+
+('545', 'Poirier', 'Nina', 'ABE', 'Chef Projet', '+33633390123'),
+
+('552', 'Bertrand', 'Sohan', 'MSY', 'RH', '+33622212345'),
+
+('560', 'Rey', 'Axelle', 'WMN', 'Data Analyst', '+33622223456'),
+
+('567', 'Marques', 'Owen', 'MKR', 'Conseiller', '+33622234567'),
+
+('574', 'Le Gall', 'Eva', 'TMM', 'Dev IT', '+33622245678'),
+
+('581', 'Vincent', 'Nolan', 'ANM', 'Chef Projet', '+33622256789'),
+
+('588', 'Blondel', 'Alix', 'APA', 'RH', '+33622267890'),
+
+('595', 'Langlois', 'Liam', 'FNR', 'Data Analyst', '+33622278901'),
+
+('612', 'Hubert', 'Clémence', 'SBV', 'Conseiller', '+33622289012'),
+
+('620', 'Gimenez', 'Milan', 'ABE', 'Dev IT', '+33622290123'),
+
+('628', 'Delaunay', 'Zélie', 'MSY', 'Chef Projet', '+33611112345'),
+
+('635', 'Guerry', 'Gabin', 'WMN', 'RH', '+33611123456'),
+
+('642', 'Adam', 'Margot', 'MKR', 'Data Analyst', '+33611134567'),
+
+('651', 'Riviere', 'Kylian', 'TMM', 'Conseiller', '+33611145678'),
+
+('658', 'Parent', 'Alicia', 'ANM', 'Dev IT', '+33611156789'),
+
+('664', 'Besson', 'Maël', 'APA', 'Chef Projet', '+33611167890'),
+
+('672', 'Julien', 'Clara', 'FNR', 'RH', '+33611178901'),
+
+('680', 'Schneider', 'Naël', 'SBV', 'Data Analyst', '+33611189012'),
+
+('688', 'Leger', 'Louise', 'ABE', 'Conseiller', '+33611190123');
