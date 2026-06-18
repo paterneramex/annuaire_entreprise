dc53584ad54ad99af434d315b54fddaa2fe02e62
paterneramex <paterne.ramex@gmail.com>
2026-06-18 20:15:06 +0300

    maitrise de App.js et api.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,27 @@
 import React, {useEffect, useState} from 'react';
 import api from './api';
 
-/*
-  useState: stocke la liste des employés
-  useEffect: déclencheur, dès que la page React se charge, il lance automatiquement
-  la requete vers http://localhost:8000/api/employes/
-*/
+function App(){
+	const [employes, setEmployoes] = useState([]);
 
-function App() {
-  const [employes, setEmployes] = useState([]);//setEmployes seul peut modifier employes
+	useEffect(() => {
+		api.get('employes/')
+		.then(response => {
+			setEmployoes(response.data);
+		})
+		.catch(error=>console.error("no data", error));
+	}, []);
 
-  useEffect(() => {
-    //Appel de l'API Django
-    api.get('employes/').then(response => {//.then() promesse
-      setEmployes(response.data);//response object complexe renvoyé par Axios contenant toute la page
-    })
-    .catch(error => console.error("Erreur lors de la recuperation: ", error));
-  },[]);
-  return (
-    <div className="App">
-      <h1>Annuaire de l'Entreprise </h1>
-      <ul>
-        {employes.map(emp => ( //,map() boucle JSON => HTML
-            <li key={emp.matricule}>{emp.prenom} {emp.nom} - {emp.post}</li>
-          ))}
-      </ul>
-    </div>
-  );
+	return(
+		<div>
+			<h2>Le mieux qu'on puisse faire</h2>
+			<ul>
+				{employes.map(emp =>(
+					<li key={emp.matricule}> {emp.nom} {emp.prenom} - {emp.poste} </li>
+					))}
+			</ul>
+		</div>
+		);
 }
 
 export default App;
